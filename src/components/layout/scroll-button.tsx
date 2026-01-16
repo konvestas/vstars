ea95ac706a5731a225f34b8b1094ad1d70e1be5c
konvestas <devkonves@yahoo.com>
2026-01-16 12:39:46 +0300

    changed placement

--- src/components/layout/scroll-button.tsx
+++ src/components/layout/scroll-button.tsx
@@ -14,7 +14,7 @@ export default function ScrollButton({ title, scrollTo }: ScrollButtonProps){
         }
     };
     return (
-        <div className="absolute bottom-2 md:bottom-8 left-1/2 -translate-x-1/2 z-20">
+        <div className="absolute bottom-0 md:bottom-8 left-1/2 -translate-x-1/2 z-20">
             <button
                 onClick={scroll}
                 className="flex flex-col items-center text-white/80
