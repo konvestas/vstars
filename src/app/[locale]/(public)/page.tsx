39fcf77a66c810b7f677f57fca2b9d444709272d
konvestas <devkonves@yahoo.com>
2026-01-19 22:56:44 +0300

    added fleet section

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -4,20 +4,21 @@ import Image from "next/image";
 import { APIProvider } from "@vis.gl/react-google-maps";
 import NavigationBar from "@/components/layout/navigation-bar";
 import FloatingWhatsApp from "@/components/layout/floating-whatsapp";
-import OurServicesSection from "@/components/layout/service-card-c";
+import OurServicesSection from "@/components/layout/service-card-content";
 import ScrollButton from "@/components/layout/scroll-button";
 import BookingWidget from "@/app/[locale]/features/booking/_components/booking-widget";
 import Footer from "@/components/layout/footer";
+import FleetSection from "@/components/layout/fleet-card-content";
 
 
 export default function App() {
     const API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string;
 
     return (
-        <main className="relative">
+        <main className="relative w-full">
             <nav className="fixed top-0 left-0 w-full z-50"><NavigationBar/></nav>
 
-            <section className="relative w-full min-h-screen flex flex-col items-center justify-center pt-24 pb-10 gap-8">
+            <section className="relative  min-h-screen flex flex-col items-center justify-center pt-24 pb-10 gap-8">
                 {/* Background Image */}
                 <div className="absolute inset-0 -z-20">
                     <Image
@@ -31,17 +32,18 @@ export default function App() {
                 {/* Dark Overlay */}
                 <div className="absolute inset-0 bg-black/40 -z-10"></div>
                 {/* Title */}
-                <div className="w-full max-w-4xl px-4 z-10">
+                <div className=" max-w-4xl px-4 z-10">
                     <h1 className="font-lexend text-white text-xl md:text-4xl font-semilight tracking-widest uppercase drop-shadow-lg text-left">
                         Istanbul&#39;s <br/>Transfer Service
                     </h1>
                 </div>
                 {/* Widget */}
-                <div className="z-10 w-full max-w-4xl -mt-8 "><APIProvider apiKey={API_KEY}><BookingWidget/></APIProvider></div>
+                <div className="z-10  max-w-4xl -mt-8 "><APIProvider apiKey={API_KEY}><BookingWidget/></APIProvider></div>
                 <div className="z-10 mt-auto md:mt-8"><ScrollButton title="View our services" scrollTo="our-services"/></div>
             </section>
 
-            <section className="w-full min-h-screen"><OurServicesSection/></section>
+            <section className="min-h-1/2"><OurServicesSection/></section>
+            <section className="min-h-1/2"><FleetSection/></section>
 
                 <footer><Footer/></footer>
             <FloatingWhatsApp/>
