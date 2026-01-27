73a4b1d87bafa69c4514630ac56b06cf37981425
konvestas <devkonves@yahoo.com>
2026-01-28 00:08:27 +0300

    fixed page imports

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -3,13 +3,13 @@
 import Image from "next/image";
 import { APIProvider } from "@vis.gl/react-google-maps";
 import NavigationBar from "@/components/layout/navigation-bar";
-import BookingWidget from "@/app/[locale]/features/booking/_components/booking-widget";
+import BookingWidget from "@/features/booking/_components/booking-widget";
 import ScrollButton from "@/components/layout/scroll-button";
-import OurServicesSection from "@/components/layout/service-card-content";
-import ChooseUs from "@/components/layout/choose-us";
+import HomeServicesSection from "@/components/homepage/home-services-section";
+import ChooseUs from "@/components/homepage/choose-us";
 import FloatingWhatsApp from "@/components/layout/floating-whatsapp";
 import Footer from "@/components/layout/footer";
-import FleetSection from "@/components/layout/fleet-card-content";
+import FleetSection from "@/components/homepage/fleet-card-content";
 
 
 export default function App() {
@@ -18,7 +18,6 @@ export default function App() {
     return (
         <main className="relative w-full">
             <nav className="fixed top-0 left-0 w-full z-50"><NavigationBar/></nav>
-
             <section className="relative min-h-screen flex flex-col items-center justify-center pt-30 pb-40 gap-8">
                 {/* Background Image */}
                 <div className="absolute inset-0 -z-20">
@@ -37,7 +36,7 @@ export default function App() {
                 <div className=" max-w-4xl px-4 z-10">
                     <h1 className="font-lexend text-white text-xl md:text-4xl font-semilight tracking-widest
                     uppercase drop-shadow-lg text-left">
-                        Istanbul&#39;s <br/>Transfer Service
+                        ISTANBUL&#39;S <br/>TRANSFER SERVICE
                     </h1>
                 </div>
                 {/* Widget */}
@@ -46,7 +45,7 @@ export default function App() {
                 <div className="z-10 mt-auto md:mt-8"><ScrollButton title="View our services" scrollTo="our-services"/></div>
             </section>
 
-            <section className="min-h-1/2"><OurServicesSection/></section>
+            <section className="min-h-1/2"><HomeServicesSection/></section>
             <section className="min-h-1/2"><ChooseUs/></section>
             <section className="min-h-1/2"><FleetSection/></section>
 
