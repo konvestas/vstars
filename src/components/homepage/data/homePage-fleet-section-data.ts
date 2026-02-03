0f5671d1be067f1c30d9bbff6a99c34c4735ca0d
konvestas <devkonves@yahoo.com>
2026-02-03 22:53:49 +0300

    changed translations place

--- src/components/homepage/data/homePage-fleet-section-data.ts
+++ src/components/homepage/data/homePage-fleet-section-data.ts
@@ -11,27 +11,27 @@ export const getFleetImages = [
 
 export const getFleetSectionData = (t:any): IFleetSectionData[]  => [
     {
-        title: t("items.new.capacityTitle"),
-        description: t("items.new.capacityDesc"),
+        title: t("items.details.capacityTitle"),
+        description: t("items.details.capacityDesc"),
     },
     {
-        title: t("items.new.luggageTitle"),
-        description: t("items.new.luggageDesc"),
+        title: t("items.details.luggageTitle"),
+        description: t("items.details.luggageDesc"),
     },
     {
-        title: t("items.new.AmbianceTitle"),
-        description: t("items.new.AmbianceDesc"),
+        title: t("items.details.AmbianceTitle"),
+        description: t("items.details.AmbianceDesc"),
     },
     {
-        title: t("items.new.ConnectivityTitle"),
-        description: t("items.new.ConnectivityDesc"),
+        title: t("items.details.ConnectivityTitle"),
+        description: t("items.details.ConnectivityDesc"),
     },
     {
-        title: t("items.new.RefreshmentsTitle"),
-        description: t("items.new.RefreshmentsDesc"),
+        title: t("items.details.RefreshmentsTitle"),
+        description: t("items.details.RefreshmentsDesc"),
     },
     {
-        title: t("items.new.PowerTitle"),
-        description: t("items.new.PowerDesc"),
+        title: t("items.details.PowerTitle"),
+        description: t("items.details.PowerDesc"),
     },
 ];
