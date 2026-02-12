d26063ff078dad4f765192914910993ddb654d68
konvestas <devkonves@yahoo.com>
2026-02-12 23:27:34 +0300

    updated

--- src/features/booking/lib/pricing-data.ts
+++ src/features/booking/lib/pricing-data.ts
@@ -1,109 +1,112 @@
+// pricing-data.ts
 import { PricingRule } from "./types";
+
 export const GREETING_FEE = 200;
 export const HOURLY_RATE = 750;
-// Default fallback prices when no zone matches
 export const DEFAULT_AIRPORT_PRICE = 2800;
 export const DEFAULT_GENERAL_PRICE = 2500;
 
 export const PRICING_ZONES: PricingRule[] = [
     {
-        regions: ["maltepe/istanbul", "kartal/istanbul", "pendik/istanbul", "sultanbeyli/istanbul", "çekmeköy/istanbul"],
+        regions: ["maltepe/istanbul", "kartal/istanbul", "pendik/istanbul", "sultanbeyli/istanbul", "cekmekoy/istanbul"],
         price: 1800
     },
     {
-        regions: ["bostancı/istanbul", "kadıköy/istanbul", "beykoz/istanbul"],
+        regions: ["bostanci/istanbul", "kadikoy/istanbul", "beykoz/istanbul"],
         price: 2300
     },
     {
-        regions: ["tuzla/istanbul", "çayırova/istanbul", "gebze/istanbul"],
+        regions: ["tuzla/istanbul", "cayirova/istanbul", "gebze/istanbul"],
         price: 2300
     },
     {
-        regions: ["şile/istanbul"],
+        regions: ["sile/istanbul"],
         price: 3300
     },
     {
-        regions: ["şişli/istanbul", "kağıthane/istanbul", "taksim/istanbul"],
+        regions: ["sisli/istanbul", "kagithane/istanbul", "taksim/istanbul"],
         price: 2200
     },
     {
-        regions: ["beşiktaş/istanbul", "beyoğlu/istanbul", "eminönü/istanbul", "sirkeci/istanbul"],
+        regions: ["besiktas/istanbul", "beyoglu/istanbul", "eminonu/istanbul", "sirkeci/istanbul"],
         price: 2200
     },
     {
-        regions: ["bağcılar/istanbul", "bakırköy/istanbul", "zeytinburnu/istanbul"],
+        regions: ["bagcilar/istanbul", "bakirkoy/istanbul", "zeytinburnu/istanbul"],
         price: 2550
     },
     {
-        regions: ["avcılar/istanbul", "esenyurt/istanbul", "büyükçekmece/istanbul"],
+        regions: ["avcilar/istanbul", "esenyurt/istanbul", "buyukcekmece/istanbul"],
         price: 2000
     }
 ];
+
 export const IST_PRICING_ZONES: PricingRule[] = [
     {
-        regions: ["maltepe/istanbul", "kartal/istanbul", "sultanbeyli/istanbul", "çekmeköy/istanbul"],
+        regions: ["maltepe/istanbul", "kartal/istanbul", "sultanbeyli/istanbul", "cekmekoy/istanbul"],
         price: 2550
     },
     {
-        regions: ["bostancı/istanbul", "kadıköy/istanbul", "beykoz/istanbul"],
+        regions: ["bostanci/istanbul", "kadikoy/istanbul", "beykoz/istanbul"],
         price: 2300
     },
     {
-        regions: ["tuzla/istanbul", "pendik/istanbul", "çayırova/istanbul", "gebze/istanbul"],
+        regions: ["tuzla/istanbul", "pendik/istanbul", "cayirova/istanbul", "gebze/istanbul"],
         price: 2800
     },
     {
-        regions: ["şile/istanbul"],
+        regions: ["sile/istanbul"],
         price: 3100
     },
     {
-        regions: ["şişli/istanbul", "kağıthane/istanbul", "taksim/istanbul"],
+        regions: ["sisli/istanbul", "kagithane/istanbul", "taksim/istanbul"],
         price: 2550
     },
     {
-        regions: ["beşiktaş/istanbul", "beyoğlu/istanbul", "eminönü/istanbul", "sirkeci/istanbul"],
+        regions: ["besiktas/istanbul", "beyoglu/istanbul", "eminonu/istanbul", "sirkeci/istanbul"],
         price: 2550
     },
     {
-        regions: ["bağcılar/istanbul", "bakırköy/istanbul", "zeytinburnu/istanbul"],
+        regions: ["bagcilar/istanbul", "bakirkoy/istanbul", "zeytinburnu/istanbul"],
         price: 2050
     },
     {
-        regions: ["avcılar/istanbul", "esenyurt/istanbul", "büyükçekmece/istanbul"],
+        regions: ["avcilar/istanbul", "esenyurt/istanbul", "buyukcekmece/istanbul"],
         price: 1800
     }
 ];
+
 export const SAW_PRICING_ZONES: PricingRule[] = [
     {
-        regions: ["maltepe/istanbul", "kartal/istanbul", "pendik/istanbul", "sultanbeyli/istanbul", "çekmeköy/istanbul"],
+        regions: ["maltepe/istanbul", "kartal/istanbul", "pendik/istanbul", "sultanbeyli/istanbul", "cekmekoy/istanbul"],
         price: 1800
     },
     {
-        regions: ["bostancı/istanbul", "kadıköy/istanbul", "beykoz/istanbul"],
+        regions: ["bostanci/istanbul", "kadikoy/istanbul", "beykoz/istanbul"],
         price: 2300
     },
     {
-        regions: ["tuzla/istanbul", "çayırova/istanbul", "gebze/istanbul"],
+        regions: ["tuzla/istanbul", "cayirova/istanbul", "gebze/istanbul"],
         price: 2300
     },
     {
-        regions: ["şile/istanbul"],
+        regions: ["sile/istanbul"],
         price: 3300
     },
     {
-        regions: ["şişli/istanbul", "kağıthane/istanbul", "taksim/istanbul"],
+        regions: ["sisli/istanbul", "kagithane/istanbul", "taksim/istanbul"],
         price: 2300
     },
     {
-        regions: ["beşiktaş/istanbul", "beyoğlu/istanbul", "eminönü/istanbul", "sirkeci/istanbul"],
+        regions: ["besiktas/istanbul", "beyoglu/istanbul", "eminonu/istanbul", "sirkeci/istanbul"],
         price: 2300
     },
     {
-        regions: ["bağcılar/istanbul", "bakırköy/istanbul", "zeytinburnu/istanbul"],
+        regions: ["bagcilar/istanbul", "bakirkoy/istanbul", "zeytinburnu/istanbul"],
         price: 2550
     },
     {
-        regions: ["avcılar/istanbul", "esenyurt/istanbul", "büyükçekmece/istanbul"],
+        regions: ["avcilar/istanbul", "esenyurt/istanbul", "buyukcekmece/istanbul"],
         price: 2800
     }
 ];
