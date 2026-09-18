a0a74ca13c0ceb6d428f3dd0709953a3490ad4e4
Konves <devkonves@gmail.com>
2026-09-18 16:29:23 +0300

    updated web app manifest with enhanced metadata, added scope, display overrides, orientation, and screenshots for improved functionality and compatibility

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,14 +2,29 @@ import { MetadataRoute } from 'next';
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
+        id: "/",
         name: 'Vstars Transfer',
         short_name: 'Vstars',
         description: "Premium VIP transfer services in Istanbul for business and leisure travelers, including private airport transfers with stress-free arrivals & departures " +
             "also chauffeured city travel and medical tourism",
+        lang: "en",
+        dir: "ltr",
+        categories: ["transportation", "business"],
         start_url: '/en',
+        scope: "/",
         display: 'standalone',
+        display_override: [
+            "window-controls-overlay",
+            "standalone",
+            "minimal-ui"
+        ],
+        orientation: "landscape-primary",
         background_color: '#FFFFFF',
         theme_color: '#FFFFFF',
+        prefer_related_applications: false,
+        launch_handler: {
+            "client_mode": "focus-existing"
+        },
         icons: [
             {
                 src: '/favicon.ico',
@@ -30,5 +45,21 @@ export default function manifest(): MetadataRoute.Manifest {
                 purpose: "maskable"
             },
         ],
+        screenshots: [
+            {
+                "src": "/screenshot-mobile.png",
+                "sizes": "402x874",
+                "type": "image/png",
+                "form_factor": "narrow",
+                "platform": "ios"
+            },
+            {
+                "src": "/screenshot-desktop.png",
+                "sizes": "1920x1080",
+                "type": "image/png",
+                "form_factor": "wide",
+                "platform":"windows",
+            }
+        ]
     };
 }
